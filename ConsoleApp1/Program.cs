f72a4da6f6469f96998122eea841b13550844d3f
branrile09 <93716194+branrile09@users.noreply.github.com>
2023-06-27 20:32:29 +0930

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Console.WriteLine("Welcome to Word Jumble!");
+            Console.WriteLine("Unscramble the letters to form a word. Can you solve all 10 puzzles?");
+
+            // list of words to be jumbled
+            List<string> words = new List<string> { "python", "computer", "keyboard", "mouse", "monitor", "program", "code", "algorithm", "variable", "debug" };
+
+            while (words.Count > 0)
+            {
+                // randomly select a word from the list
+                Random random = new Random();
+                int index = random.Next(words.Count);
+                string word = words[index];
+
+                // create a jumbled version of the word
+                char[] letters = word.ToCharArray();
+                random = new Random();
+                for (int i = 0; i < letters.Length; i++)
+                {
+                    int j = random.Next(i, letters.Length);
+                    char temp = letters[i];
+                    letters[i] = letters[j];
+                    letters[j] = temp;
+                }
+                string jumbledWord = new string(letters);
+
+                // remove the selected word from the list
+                words.RemoveAt(index);
+
+                // initialize the number of guesses to 0
+                int numGuesses = 0;
+
+                while (true)
+                {
+                    // display the jumbled word and get the user's guess
+                    Console.WriteLine($"Unscramble the word: {jumbledWord}");
+                    Console.Write("Enter your guess: ");
+                    string guess = Console.ReadLine().ToLower();
+
+                    // increment the number of guesses
+                    numGuesses++;
+
+                    // check if the guess is correct
+                    if (guess == word)
+                    {
+                        Console.WriteLine($"Congratulations! You unscrambled the word in {numGuesses} guesses.\n");
+                        break;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Sorry, that's not correct. Try again.\n");
+                    }
+                }
+            }
+
+
+
+        }
+    }
+}
---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,71 @@
+﻿namespace ConsoleApp2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            bool validInput = false;
+            int number = 0;
+            float fNumb = 0.0f;
+
+            Console.Write("enter an intiger:");
+            while (!validInput)
+            {
+                string input = Console.ReadLine();                
+                if (int.TryParse(input, out number))
+                {                   
+                    validInput = true;
+                    Console.WriteLine($"you entered {number}");
+                }
+                else
+                {
+                    Console.Clear();
+                    Console.Write("Bad input, enter an intiger:");
+                }
+            }
+           
+            do
+            {
+                Console.Write("enter a float:");
+                string input = Console.ReadLine();
+                if (float.TryParse(input, out float result))
+                {
+                    //fNumb = float.Parse(input);
+                    fNumb = result;
+                    validInput = true;
+                    Console.WriteLine($"you entered {number}");
+
+                }
+                else
+                {
+                    validInput = false;
+                    Console.Clear();
+                    Console.Write("Bad input, enter an intiger:");
+                }
+
+
+            } while (!validInput);
+
+
+            Console.WriteLine($"lets count to {number}");
+            // loops
+            for (int i = 0; i < number; i++)
+            {
+                Console.WriteLine(i);
+            }
+
+            Console.WriteLine("Computers start at 0!!!");
+            Console.WriteLine("lets try that again");
+            Console.WriteLine($"lets count to {fNumb}");
+
+
+            for (int i = 0; i < fNumb + 1; i++)
+            {
+                Console.WriteLine(i);
+            }
+
+
+
+        }
+    }
+}
